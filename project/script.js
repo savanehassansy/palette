f8b633eb68e08033e2c81bc9f04f4a0e5fe23da6
savanehassan <hassan.sy@afriqasoft.com>
2023-06-27 11:39:37 +0000

    first commit

---
+++ project/script.js
@@ -0,0 +1,41 @@
+const container = document.querySelector(".ctn");
+const refreshBtn = document.querySelector(".btn");
+
+const maxPaletteBoxes = 64;
+
+const generatePAlette = () => {
+  container.innerHTML = "";
+  for (let i = 0; i < maxPaletteBoxes; i++) {
+    let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
+    randomHex = `#${randomHex.padStart(6, "0")}`;
+    console.log(randomHex);
+
+    const color = document.createElement("li");
+    color.classList.add("color");
+    color.innerHTML = `<div
+          class="w-44 h-44 bg-[${randomHex}] rounded  active:animate-ping "
+        ></div>
+        <span class="hex m-3 font-medium text-lg uppercase text-gray-500 block text-center">${randomHex}</span>`;
+
+    color.addEventListener("click", () => copyColor(color, randomHex));
+    container.appendChild(color);
+  }
+};
+generatePAlette();
+
+const copyColor = (elem, hexVal) => {
+  const colorElement = elem.querySelector(".hex");
+  navigator.clipboard.writeText(hexVal).then(() => {
+    colorElement.innerText = "Copié";
+    setTimeout(() => (colorElement.innerHTML = hexVal), 1000);
+  });
+};
+refreshBtn.addEventListener("click", generatePAlette);
+setInterval(function () {
+  const clock = document.querySelector(".display");
+  let time = new Date();
+  let sec = time.getSeconds();
+  let min = time.getMinutes();
+  let hr = time.getHours();
+  clock.textContent = hr + ":" + min + ":" + sec;
+});
