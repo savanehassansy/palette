6083658f9dd350b1af410eca87891028f5c0ba4f
savanehassan <hassan.sy@afriqasoft.com>
2023-07-05 15:32:20 +0000

    ajout de la page de bienvenue

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const container = document.querySelector(".ctn");
 const refreshBtn = document.querySelector(".btn");
+const loader = document.querySelector("loader");
+
+
+window.addEventListener('load',() => {
+  loader.classList.add
+})
 
 //variables pour le nbre de palettes a afficher
 let maxPaletteBoxes;
@@ -14,20 +20,28 @@ getNum();
 //fonction pour générer les palettes
 const generatePAlette = () => {
   container.innerHTML = "";
-  for (let i = 0; i < maxPaletteBoxes; i++) {
-    let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
-    randomHex = `#${randomHex.padStart(6, "0")}`;
-    console.log(randomHex);
+  if (maxPaletteBoxes <= 10000) {
+    for (let i = 0; i < maxPaletteBoxes; i++) {
+      let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
+      randomHex = `#${randomHex.padStart(6, "0")}`;
+      console.log(randomHex);
 
-    const color = document.createElement("li");
-    color.classList.add("color");
-    color.innerHTML = `<div
+      const color = document.createElement("li");
+      color.classList.add("color");
+      color.innerHTML = `<div
           class="w-44 h-44 bg-[${randomHex}] rounded  active:animate-ping "
         ></div>
         <span class="hex m-3 font-medium text-lg uppercase text-gray-500 block text-center">${randomHex}</span>`;
 
-    color.addEventListener("click", () => copyColor(color, randomHex));
-    container.appendChild(color);
+      color.addEventListener("click", () => copyColor(color, randomHex));
+      container.appendChild(color);
+    }
+  } else {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: `Désolez nous ne pouvons pas fournir  ${maxPaletteBoxes} palettes à la fois, veuillez entrer un nombre en dessous de 10 000`,
+    });
   }
 };
 
@@ -42,7 +56,7 @@ const copyColor = (elem, hexVal) => {
   elem.querySelector(".hex");
   navigator.clipboard.writeText(hexVal).then(() => {
     Swal.fire({
-      width:300,
+      width: 300,
       icon: "success",
       title: "Couleur copié",
       showConfirmButton: false,
@@ -60,6 +74,3 @@ setInterval(function () {
   let hr = time.getHours();
   clock.textContent = hr + ":" + min + ":" + sec;
 });
-
-
-
